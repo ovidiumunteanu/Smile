f0ee64ce6bafb4b586399cf1d1f5c0068bd88d00
Shendi_Qerimi <Danevhome01@gmail.com>
2020-10-19 21:06:24 -0700

    Lunch Screen and Logout button

--- src/Loading.js
+++ src/Loading.js
@@ -1,29 +1,46 @@
 // Loading.js
-import React from 'react'
-import { View, Text, ActivityIndicator, StyleSheet } from 'react-native'
+import React, { Component } from 'react';
+import { View, Text, StyleSheet, ImageBackground, Dimensions } from 'react-native';
 import auth from '@react-native-firebase/auth'
+
+const { width, height } = Dimensions.get("window");
 
 export default class Loading extends React.Component {
 
   componentDidMount() {
-    auth().onAuthStateChanged(user => {
-      this.props.navigation.navigate(user ? 'Main' : 'SignUp')
-    })
+    const that = this;
+    setTimeout(() =>{
+      auth().onAuthStateChanged(user => {
+        this.props.navigation.navigate(user ? 'Main' : 'Login')
+      })
+    }, 4000)
+    
   }
 
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Loading</Text>
-        <ActivityIndicator size="large" />
+      <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+        <ImageBackground style={style.container} source={require("../assets/images/Logo.png")}>
+            <View style={style.overlay}></View>
+        </ImageBackground>
       </View>
-    )
+      
+    );
   }
 }
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+
+const style = StyleSheet.create({
+
+  container : {
+      width : 300, 
+      height : 200,
+      resizeMode: "center"
+  },
+  overlay : {
+      width : width,
+      height : height,
+      position: "absolute",
+      backgroundColor: "transparent"
   }
+
 })
--- src/Main.js
+++ src/Main.js
@@ -1,7 +1,10 @@
 // Main.js
 import React from 'react'
-import { StyleSheet, Platform, Image, Text, View } from 'react-native'
+import { StyleSheet, Platform, Dimensions, Image, TouchableOpacity, Text, View } from 'react-native'
 import auth from '@react-native-firebase/auth'
+import {widthPercentageToDP as wp, heightPercentageToDP as hp} from 'react-native-responsive-screen';
+
+const { width, height } = Dimensions.get("screen");
 
 export default class Main extends React.Component {
   state = { currentUser: null }
@@ -9,14 +12,28 @@ export default class Main extends React.Component {
     const { currentUser } = auth()
     this.setState({ currentUser })
   }
+  _logout = () => {
+    auth()
+    .signOut()
+    .then(() => {
+        console.log('User signed out!');
+        this.props.navigation.navigate("Login");
+      })
+    .catch(error => {
+        
+    });
+  }
   render() {
       const { currentUser } = this.state      
       return (
-            <View style={styles.container}>
-              <Text>
-                Hi {currentUser && currentUser.email}!
-              </Text>
-            </View>
+        <View style={styles.container}>
+          <Text>
+            Hi {currentUser && currentUser.email}!
+          </Text>
+          <TouchableOpacity style={styles.submit} onPress={this._logout} activeOpacity={1}>
+              <Text style={{color : "white", fontSize : 20}}>LOGOUT</Text>
+          </TouchableOpacity>
+        </View>
       )
     }
 }
@@ -25,5 +42,15 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center'
-  }
+  },
+  submit : {
+    width :wp("80%"), 
+    height :wp("14%"), 
+    backgroundColor :"red", 
+    marginTop : 25,
+    // borderBottomLeftRadius :70,
+    borderRadius: 30, 
+    justifyContent:"center", 
+    alignItems :"center"
+  },
 })
