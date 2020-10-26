be5a6a705610d4d0b4aac38b30b6657b17dfd39a
Shendi_Qerimi <Danevhome01@gmail.com>
2020-10-26 06:23:42 -0300

    Tab Navigation was created for 4 tabs

--- src/navigations/MainStack.js
+++ src/navigations/MainStack.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import HomeScreen from "../screens/main/HomeScreen";
+import HomeScreen from "../screens/main/delivery/HomeScreen";
+import DeliveryStack from "./DeliveryStack";
+import SupplierStack from "./SupplierStack";
+import SocialStack from "./SocialStack";
+import KingdomStack from "./KingdomStack";
 // import NotificationStack from "./NotificationStack";
 // import ChatStack from "./ChatStack";
 // import ProfileStack from "./ProfileStack";
 import { useIsFocused } from "@react-navigation/native";
-import { Image } from "react-native";
+import {Text, View, StyleSheet} from "react-native";
+import FastImage from "react-native-fast-image";
 import { isIphoneX } from 'react-native-iphone-x-helper';
 
 const TabStack = createBottomTabNavigator();
@@ -16,29 +21,96 @@ function MainStack({ navigation }) {
             lazy={false}
             tabBarOptions={{
                 style: {
-                    backgroundColor: "#1F1F21",
-                    minHeight: isIphoneX()?85 : 55,
-                    paddingTop : 5,
+                    // backgroundColor: "#1F1F21",
+                    minHeight: isIphoneX()?75 : 55,
+                    paddingTop : 0,
+                    marginBottom: 0
                 },
+                showLabel: false,
                 labelStyle: {
                     fontSize: 14,
                     fontFamily: "DMSans-Medium",
-                    marginBottom: 5
+                    marginBottom: 0
                 },
                 activeTintColor: "#DA1DA2"
             }}
         >
             <TabStack.Screen
-                name="Home"
-                component={HomeScreen}
+                name="Delivery"
+                component={DeliveryStack}
                 options={{
                     title: "Delivery",
                     tabBarIcon: () => {
                         const isFocused = useIsFocused();
                         if (isFocused) {
-                            return <Image source={require("../../assets/icons/ic_truck.png")} />
+                            return (
+                                <View style={{...styles.tabView, backgroundColor: '#3938'}}>
+                                    <FastImage style={styles.tabImage} source={require("../../assets/icons/ic_truck.png")} />
+                                    <Text>Delivery</Text>
+                                </View>
+                            )
                         } else {
-                            return <Image source={require("../../assets/icons/ic_truck.png")} />
+                            return <FastImage style={styles.tabImage} source={require("../../assets/icons/ic_truck.png")} />
+                        }
+                    }
+                }}
+            />
+            <TabStack.Screen
+                name="Supplier"
+                component={SupplierStack}
+                options={{
+                    title: "Supplier",
+                    tabBarIcon: () => {
+                        const isFocused = useIsFocused();
+                        if (isFocused) {
+                            return (
+                                <View style={{...styles.tabView, backgroundColor: '#9398'}}>
+                                    <FastImage style={styles.tabImage} source={require("../../assets/icons/ic_supplier.png")} />
+                                    <Text>Supplier</Text>
+                                </View>
+                            )
+                        } else {
+                            return <FastImage style={styles.tabImage} source={require("../../assets/icons/ic_supplier.png")} />
+                        }
+                    }
+                }}
+            />
+            <TabStack.Screen
+                name="Social"
+                component={SocialStack}
+                options={{
+                    title: "Social",
+                    tabBarIcon: () => {
+                        const isFocused = useIsFocused();
+                        if (isFocused) {
+                            return (
+                                <View style={{...styles.tabView, backgroundColor: '#e009'}}>
+                                    <FastImage style={styles.tabImage} source={require("../../assets/icons/ic_smile_like.png")} />
+                                    <Text>Red Social</Text>
+                                </View>
+                            )
+                        } else {
+                            return <FastImage style={styles.tabImage} source={require("../../assets/icons/ic_smile_like.png")} />
+                        }
+                    }
+                }}
+            />
+            <TabStack.Screen
+                name="kingdom"
+                component={KingdomStack}
+                options={{
+                    title: "KINGDOM",
+                    tabBarIcon: () => {
+                        const isFocused = useIsFocused();
+                        if (isFocused) {
+                            return (
+                                <View style={{...styles.tabView, backgroundColor: '#00e7'}}>
+                                    <FastImage style={styles.tabImage} source={require("../../assets/icons/ic_crown.png")} />
+                                    <Text>KINGDOM</Text>
+                                </View>
+                            )
+                        } else {
+                            return <FastImage style={styles.tabImage} source={require("../../assets/icons/ic_crown.png")} />
                         }
                     }
                 }}
@@ -86,6 +158,30 @@ function MainStack({ navigation }) {
         </TabStack.Navigator>
     )
 }
+
+const styles = StyleSheet.create({
+
+    tabView : {
+        flexDirection:"row", 
+        alignItems:"center", 
+        justifyContent:"center", 
+        paddingLeft: 10, 
+        paddingRight: 10, 
+        paddingTop: 5,
+        paddingBottom: 5,
+        borderRadius: 30
+    },
+    tabText : {        
+        position: "absolute",
+        backgroundColor: "transparent"
+    },
+    tabImage: {
+        width: 30, 
+        height: 30, 
+        resizeMode: 'stretch'
+    }
+  
+  })
 
 export default MainStack;
 
---
+++ src/screens/main/delivery/HomeScreen.js
@@ -0,0 +1,56 @@
+// Main.js
+import React from 'react'
+import { StyleSheet, Platform, Dimensions, Image, TouchableOpacity, Text, View } from 'react-native'
+import auth from '@react-native-firebase/auth'
+import {widthPercentageToDP as wp, heightPercentageToDP as hp} from 'react-native-responsive-screen';
+
+const { width, height } = Dimensions.get("screen");
+
+export default class HomeScreen extends React.Component {
+  state = { currentUser: null }
+  componentDidMount() {
+    const { currentUser } = auth()
+    this.setState({ currentUser })
+  }
+  _logout = () => {
+    auth()
+    .signOut()
+    .then(() => {
+        console.log('User signed out!');
+        this.props.navigation.navigate("AuthStack");
+      })
+    .catch(error => {
+        
+    });
+  }
+  render() {
+      const { currentUser } = this.state      
+      return (
+        <View style={styles.container}>
+          <Text>
+            Hi {currentUser && currentUser.email}!
+          </Text>
+          <TouchableOpacity style={styles.submit} onPress={this._logout} activeOpacity={1}>
+              <Text style={{color : "white", fontSize : 20}}>LOGOUT</Text>
+          </TouchableOpacity>
+        </View>
+      )
+    }
+}
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  submit : {
+    width :wp("80%"), 
+    height :wp("14%"), 
+    backgroundColor :"red", 
+    marginTop : 25,
+    // borderBottomLeftRadius :70,
+    borderRadius: 30, 
+    justifyContent:"center", 
+    alignItems :"center"
+  },
+})
